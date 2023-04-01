1cbc4531db8f8a09c676f94681ff4a8318a6e00d
Chris <christof.jori.dev@gmail.com>
2023-04-01 21:51:15 +0200

    guest txhash

--- methods/guest/src/bin/evm.rs
+++ methods/guest/src/bin/evm.rs
@@ -21,7 +21,7 @@ use risc0_zkvm::guest::env;
 
 risc0_zkvm::guest::entry!(main);
 
-
+#[tokio::main]
 pub fn main() {
     // NOTE: env::read_slice requires a length argument. Reads must be of known
     // length. https://github.com/risc0/risc0/issues/402
@@ -34,35 +34,20 @@ pub fn main() {
     let creditor = input[2].clone().into_address().unwrap();
     let amount: U256 = input[3].clone().into_uint().unwrap();
 
-    // TODO: do stuff
+    println!("nice");
 
-    //let client = Provider::<Http>::try_from("https://eth.llamarpc.com").expect("Invalid RPC url");
-    /*let client = Arc::new(client);
+    let client = Provider::<Http>::try_from("https://eth.llamarpc.com").expect("Invalid RPC url");
+
+    let tx_hash = H256::from_str(tx_hash).expect("Invalid transaction hash");
 
     let tx = client.get_transaction(tx_hash).await.unwrap().unwrap();
-    let block_numb = tx.block_number.unwrap();
-    info!("Running TX: 0x{:x} at block {}", tx_hash, block_numb);
 
-    let mut env = Env::default();
-    env.block.number = U256::from(block_numb.as_u64());
-    env.tx = evm_core::ether_trace::txenv_from_tx(tx);
-    let trace_db = evm_core::ether_trace::TraceTx::new(client, Some(block_numb.as_u64())).unwrap();
+    println!("Block Hash {:?} received {:?}", tx_hash, tx.block_hash);
+    println!("From {:?} received {:?}", msg_sender, tx.from);
+    println!("Chain_ID {:?} received {:?}", creditor, tx.chain_id);
+    println!("Gas Price{:?} received {:?}", amount, tx.gas_price);
 
-    let mut evm = EVM::new();
-    evm.database(trace_db);
-    evm.env = env.clone();
+    // check
 
-    let mut evm = EVM::new();
-    evm.database(db);
-    evm.env = env;
-    let (res, state) = evm.transact();*/
-
-    /*
-    let success = true; // TODO: check if the transaction was successful, from to right person, correct amount, etc.
-    env::commit_slice(&ethabi::encode(&[Token::Bool(success), Token::Address(msg_sender)]));
-
-    env::log("");*/
-
-    // Commit the journal that will be decoded in the application contract.
     env::commit_slice(&ethabi::encode(&[Token::Bool(true), Token::Address(msg_sender)]));
 }
